1328708cb618ae655a17e98b6cdd10fc8b105464
Calum <calumcmccall@gmail.com>
2022-09-21 14:23:27 +0100

    mvp + extensions

---
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository.java
@@ -0,0 +1,16 @@
+package com.codeclan.example.WhiskyTracker.repositories;
+
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface WhiskyRepository extends JpaRepository<Whisky, Long> {
+
+    List<Whisky> findWhiskyByYearEquals(int year);
+
+    List<Whisky> findWhiskyByDistilleryNameAndAge(String distillery, int age);
+
+    List<Whisky> findWhiskyByDistilleryRegion(String region);
+
+}
